24f5d72a2655557f3a651a878fa8812c5f97521e
black <423748313@qq.com>
2018-06-10 19:54:11 +0800

    qunar-init

---
+++ src/components/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home"></div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      'title': 'BlackXu'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
